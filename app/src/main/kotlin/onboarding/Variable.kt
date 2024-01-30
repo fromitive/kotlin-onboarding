8ee018d07b5f713a65adfc73d61441a2791ff411
fromitive <fromitive@gmail.com>
2024-01-30 15:06:54 +0900

    refactor: 지원하는 기본 변수 타입들

--- app/src/main/kotlin/onboarding/Variable.kt
+++ app/src/main/kotlin/onboarding/Variable.kt
@@ -4,4 +4,11 @@ fun main(){
     // birth = 1993 //var은 안바꾸는게 원칙이라 경고 오류가 나타남. 값은 변경될 수 있음.
     println("My Name is " + name)
     println("Year of Birth is " + birth)
+
+    // 지원하는 기본 변수 type 들
+    val myNum       = 5             // Int
+    val myDoubleNum = 5.99          // Double
+    val myLetter    = 'D'           // Char
+    val myBoolean   = true          // Boolean
+    val myText      = "Hello"       // String
 }
