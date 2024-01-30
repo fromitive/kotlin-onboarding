0e679eb6ea3bbae98bf3c79c3a1ac8a52592639a
fromitive <fromitive@gmail.com>
2024-01-30 15:05:30 +0900

    feat: 변수 예제 추가

---
+++ app/src/main/kotlin/onboarding/Variable.kt
@@ -0,0 +1,7 @@
+fun main(){
+    var name = "Moosong"
+    var birth = 1994
+    // birth = 1993 //var은 안바꾸는게 원칙이라 경고 오류가 나타남. 값은 변경될 수 있음.
+    println("My Name is " + name)
+    println("Year of Birth is " + birth)
+}
